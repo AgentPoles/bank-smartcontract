862556fb3e19c697354ffd69cb4956694044a4f4
Agentpoles <pauloams16@gmail.com>
2022-01-09 13:00:41 -0800

    updates

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,49 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
 
+// but useful for running the script in a standalone fashion through `node <script>`.
+
+//
+
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+
+// Runtime Environment's members available in the global scope.
+
+const hre = require("hardhat");
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
+
   // line interface.
+
   //
+
   // If this script is run directly using `node` you may want to call compile
+
   // manually to make sure everything is compiled
+
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  const [owner] = await hre.ethers.getSigners();
 
-  console.log("Greeter deployed to:", greeter.address);
+  const BankContractFactory = await hre.ethers.getContractFactory("Bank");
+
+  const BankContract = await BankContractFactory.deploy();
+
+  await BankContract.deployed();
+
+  console.log("BankContract deployed to:", BankContract.address);
+
+  console.log("BankContract owner address:", BankContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
+
 // and properly handle errors.
+
 main()
   .then(() => process.exit(0))
+
   .catch((error) => {
     console.error(error);
     process.exit(1);
